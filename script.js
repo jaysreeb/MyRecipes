2cf21cfa3a971c7dddaa738a1f740d4da63f2f60
jaysreeb <134087001+jaysreeb@users.noreply.github.com>
2025-03-06 23:44:55 +0530

    API

---
+++ script.js
@@ -0,0 +1,6 @@
+// fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=chicken")
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.error("Error fetching recipes:", error));
+
+// // API key: 91f0e146806e459cb9b927f739690382
